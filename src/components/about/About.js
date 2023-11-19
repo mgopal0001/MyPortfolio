4d722f47497a8eeab56ab7cbce586e586832b7ef
Madan Gopal <madan@Satyams-MacBook-Air.local>
2023-11-19 16:31:33 +0530

    Added About section and updated resume

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,4 +1,48 @@
-const About = () =>{
-    return <>This is About page</>
-}
+import React from "react";
+import Card from "react-bootstrap/Card";
+import { ImPointRight } from "react-icons/im";
+import "./style.css";
+import AboutImage from "../../assets/about-image.png";
+
+const About = () => {
+  return (
+    <>
+      <div className="main-con">
+        <div className="about-con">
+          <div className="about-text-con">
+            <p style={{ textAlign: "justify" }}>
+              Hi Everyone, I am{" "}
+              <span className="purple"> Madan Gopal </span>
+              from <span className="purple"> Varanasi, India.</span>
+              <br />
+              I have completed Master of Computer Application from <span className="purple"><br/> KIET Group of Institutions Ghaziabad.</span>
+              <br />
+              <br />
+              Apart from coding, some other activities that I love to do!
+            </p>
+            <ul>
+              <li className="about-activity">
+                 Playing Guitar
+              </li>
+              <li className="about-activity">
+                 Writing Tech Blogs
+              </li>
+              <li className="about-activity">
+                Travelling
+              </li>
+            </ul>
+
+            <p style={{ color: "rgb(155 126 172)" }}>
+              "Strive to build things that make a difference!"{" "}
+            </p>
+          </div>
+          <div className="about-image-con">
+            <img src={AboutImage} width={700} height={450} />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
 export default About;
--- src/components/resume/Resume.js
+++ src/components/resume/Resume.js
@@ -1,8 +1,44 @@
+import { Document, Page, pdfjs } from "react-pdf";
+import React, { useState, useEffect } from "react";
+
+import Button from "react-bootstrap/Button";
+import pdf from "../../assets/MADAN-GOPAL.pdf";
+import "./style.css";
+import "react-pdf/dist/esm/Page/AnnotationLayer.css";
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
+
+
 const Resume = () => {
-    return (
-        <>
-            This is Resume
-        </>
-    )
+  const [width, setWidth] = useState(1200);
+
+  useEffect(() => {
+    setWidth(window.innerWidth);
+  }, []);
+
+  return (
+    <>
+      <div className="resume-con">
+        <Button
+          variant="primary"
+          href={pdf}
+          target="_blank"
+          style={{ maxWidth: "250px" }}
+        >
+            Download CV
+        </Button>
+        <Document file={pdf}>
+          <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} renderTextLayer={false}/>
+        </Document>
+        <Button
+          variant="primary"
+          href={pdf}
+          target="_blank"
+          style={{ maxWidth: "250px" }}
+        >
+            Download CV
+        </Button>
+      </div>
+    </>
+  );
 };
 export default Resume;
