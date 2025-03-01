3c9dd9403cc4178ed2ca3cff51c9fa691bba1b5a
Madan Gopal <madan.gopal@digitalsherpa.ai>
2025-03-02 01:13:07 +0530

    Added new projects and resume

--- src/components/projects/Constants.js
+++ src/components/projects/Constants.js
@@ -2,34 +2,40 @@ import Calculator from "../../assets/calculator.png";
 import CryptoGrad from "../../assets/cryptograd.png";
 
 export const projectDetails = [
-  {
-    title: "Cryptograd AI",
-    link: "https://beta.cryptograd.ai",
-    details:
-      "Conceived and developed the CryptoGrad AI Web application, an innovative platform designed to facilitate real-time conversations with users regarding cryptocurrency investments. ",
-  },
-  {
-    title: "ToDo Project",
-    link: "https://react-todo-project-three.vercel.app/",
-    details:
-      "The ToDo Project is a task management application designed to help individuals and teams organize and prioritize their daily activities.",
-  },
-  {
-    title: "React Calculator",
-    link: "https://react-calculator-project-iota.vercel.app/",
-    details:
-      "The React Calculator is a web application built using the React library. It serves as a basic calculator that allows users to perform arithmetic operations. ",
-  },
-  {
-    title: "IPL Data Project",
-    link: "https://js-ipl-data-project-js-ipl-data-project.vercel.app/",
-    details:
-      "Implemented a comprehensive IPL Data Project, spanning 10 years of IPL Data stored in CSV format. The primary objective was to extract, transform, and load the data by converting it into JSON format. Leveraging advanced data processing techniques, the project aimed to generate desired outputs based on specific queries.",
-  },
-  {
-    title: "IPL Data Project",
-    link: "https://js-ipl-data-project-js-ipl-data-project.vercel.app/",
-    details:
-      "Implemented a comprehensive IPL Data Project, spanning 10 years of IPL Data stored in CSV format. The primary objective was to extract, transform, and load the data by converting it into JSON format. Leveraging advanced data processing techniques, the project aimed to generate desired outputs based on specific queries.",
-  },
+	{
+		title: "Cryptograd AI",
+		link: "https://beta.cryptograd.ai",
+		details:
+			"Conceived and developed the CryptoGrad AI Web application, an innovative platform designed to facilitate real-time conversations with users regarding cryptocurrency investments. ",
+	},
+	{
+		title: "ToDo Project",
+		link: "https://react-todo-project-three.vercel.app/",
+		details:
+			"The ToDo Project is a task management application designed to help individuals and teams organize and prioritize their daily activities.",
+	},
+	{
+		title: "React Calculator",
+		link: "https://react-calculator-project-iota.vercel.app/",
+		details:
+			"The React Calculator is a web application built using the React library. It serves as a basic calculator that allows users to perform arithmetic operations. ",
+	},
+	{
+		title: "IPL Data Project",
+		link: "https://js-ipl-data-project-js-ipl-data-project.vercel.app/",
+		details:
+			"Implemented a comprehensive IPL Data Project, spanning 10 years of IPL Data stored in CSV format. The primary objective was to extract, transform, and load the data by converting it into JSON format. Leveraging advanced data processing techniques, the project aimed to generate desired outputs based on specific queries.",
+	},
+	{
+		title: "Real Estate Property Listing Platform",
+		link: "https://z-india-realty.com/",
+		details:
+			"I built a real estate property listing platform using React, where users can explore properties dynamically with a carousel and detailed project pages. It enhanced user experience by implementing smooth navigation, optimized state management, and a responsive UI.",
+	},
+	{
+		title: "CSV to JSON Converter",
+		link: "https://github.com/mgopal0001/csvToJson",
+		details:
+			"I developed a CSV-to-JSON converter using Node.js and Express, allowing users to upload CSV files and get structured JSON output instantly. This project improved data processing efficiency and simplified data integration for developers.",
+	},
 ];
